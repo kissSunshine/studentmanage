132748b6b4641f9a910f21bd0ec281160b3144a5
sunshine <1099147061@qq.com>
2021-11-27 21:41:58 +0800

    活动管理-更新活动

--- src/main/java/com/zh/studentmanage/vo/ActivityRealTeacherVo.java
+++ src/main/java/com/zh/studentmanage/vo/ActivityRealTeacherVo.java
@@ -1,65 +1,25 @@
 package com.zh.studentmanage.vo;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.zh.studentmanage.enums.AttendEnum;
-import com.zh.studentmanage.enums.StatusEnum;
-import com.zh.studentmanage.utils.EnumUtil;
 import lombok.Data;
 
-import java.io.Serializable;
-import java.util.Date;
+import java.util.List;
 
 @Data
 public class ActivityRealTeacherVo {
+
     /**
      * Act + 32位UUID，不同活动唯一标识
      */
     private String activityid;
-    private String activityName;
 
     /**
-     * Tea + 32位UUID，教师身份唯一标识
-     */
-    private String teacherid;
-    private String teacherName;
-
-    /**
-     * Sch + 12位雪花号，校区身份唯一标识
+     * Sch + 32位雪花号，校区身份唯一标识
      */
     private String schoolid;
-    private String schoolName;
 
     /**
-     * 开始日期:形如 2021-10-01
+     * 存放每个活动下不同校区的多个教师
      */
-    private String startdate;
-    /**
-     * 结束日期:形如 2021-10-01
-     */
-    private String enddate;
-    /**
-     * 0-未参加；1-已参加
-     */
-    private Integer attend;
-    private String attendName;
-
-    /**
-     * 创建时间
-     */
-    private Date createdTime;
-    /**
-     * 修改时间
-     */
-    private Date updatedTime;
-    /**
-     * 修改信息操作人员
-     */
-    private String updatedPerson;
-
-    @JsonIgnore
-    public AttendEnum  getAttendEnum(){
-        return EnumUtil.getEnumName(attend, AttendEnum.class);
-    }
-
+    private List<ActivityRealTeacherDetailVo> activityRealTeacherDetailVo;
 }
 
