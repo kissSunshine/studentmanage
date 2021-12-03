b7ba8a35829c2cedda986de79f43b12d7c4bf560
Alvis <zhanghao08@sinosoft.com.cn>
2021-12-03 21:36:27 +0800

    教师管理

--- src/main/java/com/zh/studentmanage/controller/TeacherController.java
+++ src/main/java/com/zh/studentmanage/controller/TeacherController.java
@@ -1,12 +1,17 @@
 package com.zh.studentmanage.controller;
 
+import com.zh.studentmanage.enums.PositionEnum;
 import com.zh.studentmanage.pojo.Teacher;
 import com.zh.studentmanage.service.TeacherService;
 import com.zh.studentmanage.vo.ResponseVo;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/teacher")
@@ -38,7 +43,27 @@ public class TeacherController {
     @GetMapping("/queryByParams")
     public ResponseVo<List<Teacher>> queryByParams(Teacher teacher) {
         List<Teacher> teacherList = teacherService.queryByParam(teacher);
-        return ResponseVo.success("查询班主任老师成功", teacherList);
+        return ResponseVo.success("查询成功", teacherList);
+    }
+
+    @GetMapping("/querySubjectTeacher")
+    public ResponseVo<List<String[]>> querySubjectTeacher() {
+        // 查询学科教师，职位为班主任和教师
+        Teacher teacher = new Teacher();
+        // 班主任
+        teacher.setPosition(PositionEnum.CLASSES_DIRECTOR.getCode());
+        List<Teacher> directorList = teacherService.queryByParam(teacher);
+        // 教师
+        teacher.setPosition(PositionEnum.TEACHER.getCode());
+        List<Teacher> teacherList = teacherService.queryByParam(teacher);
+        // 合并
+        directorList.addAll(teacherList);
+
+        List<String[]> subjectTeacherList = directorList.stream().map(one -> {
+            return new String[]{one.getId(), one.getNickname(), one.getSubject()};
+        }).collect(Collectors.toList());
+
+        return ResponseVo.success("查询成功", subjectTeacherList);
     }
 
 }
