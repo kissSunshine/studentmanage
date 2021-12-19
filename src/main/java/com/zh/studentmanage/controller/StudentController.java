994912f939368dd287f14f038a6079d6db823155
三又十二分之四 <1099147061@qq.com>
2021-12-20 00:04:57 +0800

    学生管理，上传下载

--- src/main/java/com/zh/studentmanage/controller/StudentController.java
+++ src/main/java/com/zh/studentmanage/controller/StudentController.java
@@ -1,7 +1,9 @@
 package com.zh.studentmanage.controller;
 
 import cn.afterturn.easypoi.excel.ExcelExportUtil;
+import cn.afterturn.easypoi.excel.ExcelImportUtil;
 import cn.afterturn.easypoi.excel.entity.ExportParams;
+import cn.afterturn.easypoi.excel.entity.ImportParams;
 import cn.afterturn.easypoi.excel.entity.enmus.ExcelType;
 import com.zh.studentmanage.enums.PageEnum;
 import com.zh.studentmanage.excelexport.StudentExport;
@@ -12,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import javax.servlet.ServletOutputStream;
@@ -31,7 +34,7 @@ public class StudentController {
     private StudentService studentService;
 
     @PostMapping("/add")
-    public ResponseVo add(@RequestBody Student student){
+    public ResponseVo add(@RequestBody Student student) {
         return studentService.insert(student);
     }
 
@@ -41,12 +44,12 @@ public class StudentController {
     }
 
     @PostMapping("/delete")
-    public ResponseVo<String> delete(@RequestBody Student student){
+    public ResponseVo<String> delete(@RequestBody Student student) {
         return studentService.deleteById(student.getId());
     }
 
     @PostMapping("/update")
-    public ResponseVo<String> update(@RequestBody Student student){
+    public ResponseVo<String> update(@RequestBody Student student) {
         return studentService.update(student);
     }
 
@@ -62,16 +65,16 @@ public class StudentController {
         ServletOutputStream out = null;
         try {
             // 流形式传输
-            response.setHeader("content-type","application/octet-stream");
+            response.setHeader("content-type", "application/octet-stream");
             // 防止中文乱码
             response.setHeader("content-disposition", "attachment;filename=" + URLEncoder.encode("员工表.xls", "UTF-8"));
             // 将content-disposition暴露给前端，前端代码才可以拿到值
-            response.setHeader("Access-Control-Expose-Headers","content-disposition");
+            response.setHeader("Access-Control-Expose-Headers", "content-disposition");
             out = response.getOutputStream();
             workbook.write(out);
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             if (null != out) {
                 try {
                     out.close();
@@ -84,4 +87,17 @@ public class StudentController {
         return ResponseVo.success("导出成功");
     }
 
+    @PostMapping("/import")
+    public ResponseVo<String> importStudents(@RequestParam("file")MultipartFile multipartFile) {
+        ImportParams importParams = new ImportParams();
+        // 标题行
+        importParams.setTitleRows(1);
+        try {
+            List<StudentExport> studentExports = ExcelImportUtil.importExcel(multipartFile.getInputStream(), StudentExport.class, importParams);
+            studentExports.forEach(System.out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
